e450b9e1a58100f53988bc0c06b0c72b1169b4d4
Roystone <80714683+Roystone-Were@users.noreply.github.com>
2024-11-13 08:35:19 +0300

    "Local Storage Setup"

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ const list = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 const saveInputButton = document.getElementById("input-btn");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+//prevents data lose after refresh
+if(leadsFromLocalStorage){
+    myLeads =leadsFromLocalStorage
+    renderLeads()
+}
+
+//click event listener function
 saveInputButton.addEventListener("click",renderLeads)
 
 function renderLeads(){
     myLeads.push(inputEl.value)
 
-    list.innerHTML= ""
+    list.innerHTML= "" //prevents data duplicate after every render
+
+    //save to localStorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log(localStorage.getItem("myLeads"))
 
     for (let i = 0; i < myLeads.length; i++) {
         let li = document.createElement("li")
-        // li.innerText += myLeads[i]
-        // document.getElementById("ul-el").appendChild(li)
         let a = document.createElement("a")
         a.href = myLeads[i]
         a.textContent = myLeads[i]
